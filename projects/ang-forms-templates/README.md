b2f1fcb5e7f3429701bc30ab7b98573ca0af1c1a
narasimhavuppala <nraovuppala@Wgmail.com>
2018-08-19 06:33:22 +0530

    Added validation logic for the template driven forms

---
+++ projects/ang-forms-templates/README.md
@@ -0,0 +1,10 @@
+# Angular Template Driven Forms
+ - This Project covers
+   - ngForm Directive
+     - This requires name mandatory field
+   - Validation
+   - css classs for:
+     - touched
+     - Changed
+     - Valid
+     
--- projects/ang-forms-templates/src/app/app.component.ts
+++ projects/ang-forms-templates/src/app/app.component.ts
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  username: String;
-  password: String;
+  username: String = '';
+  password: String = '';
+
+  onSubmit() {
+    console.log(this.username);
+    console.log(this.password);
+  }
 }
